62d2e91c56c287a393f1066edf8ba1a92494886e
Ken Coles <kenscoles@gmail.com>
2024-05-06 13:20:27 +0100

    changes

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,7 +3,8 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: "counter",
   standalone: true,
-  template: ` <h1>Counter value: {{ value() }}</h1>`,
+  template: ` <div class="col"><h1>Counter value: {{ value() }}</h1></div>`,
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
   value = input.required<number>();
