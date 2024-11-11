dc0fa613197a6857636d38ac9941e37a6dc1e116
Ken Coles <kenscoles@gmail.com>
2024-11-11 16:32:25 +0000

    removed photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component'
+import {MatButtonModule} from '@angular/material/button'
+import { sum } from './util';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [ CounterComponent, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frb24';
-  count=10;
+  count=0;
+  value = signal(this.count+4)
 
   inc(){
     this.count +=1
+    this.value.set(sum(this.count,4))
   }
 
   dec(){
     this.count -=1
+    this.value.set(sum(this.count,4))
   }
+ 
+  //this.value = sum(this.count,4)
+  
 }
+// ng deploy --base-href=/Frb24/
